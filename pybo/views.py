768964c4b848276f31972e61a357cc6bc4d04755
Jay <kubby159@naver.com>
2022-09-18 00:41:35 +0900

    질문등록하기

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Question, Answer
+from .form import QuestionForm
 
 
 def index(request):
@@ -20,3 +21,16 @@ def answer_create(request, question_id):
     answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
     answer.save()
     return redirect('pybo:detail', question_id = question.id)
+
+def question_create(request):
+    if request.method =="POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
