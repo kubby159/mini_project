5a317bb3239df6eb69987b4ce170d0b94870f664
Jay <kubby159@naver.com>
2022-09-28 00:38:17 +0900

    글쓴이추가

--- pybo/views.py
+++ pybo/views.py
@@ -5,7 +5,7 @@ from .models import Question, Answer
 from django.http import HttpResponseNotAllowed
 from .form import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
 def index(request):
     page = request.GET.get('page','1') #페이지
     question_list = Question.objects.order_by('-create_date')
@@ -21,6 +21,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     '''
     pybo 답변등록
@@ -30,12 +31,13 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user # author 속성에 로그인 계정 저장.
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible')
+        form = AnswerForm()
     context = {'question': question, 'form':form}
     return render(request, 'pybo/question_detail.html', context)
 
@@ -45,11 +47,13 @@ def answer_create(request, question_id):
     answer.save()
     return redirect('pybo:detail', question_id = question.id)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method =="POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user # author 속성에 로그인 계정 저장.
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
