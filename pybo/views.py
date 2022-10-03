8ec5e9754431b708cc5a28456c1847fb8035ae99
Jay <kubby159@naver.com>
2022-10-03 22:43:09 +0900

    수정버튼

--- pybo/views.py
+++ pybo/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseNotAllowed
 from .form import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 def index(request):
     page = request.GET.get('page','1') #페이지
     question_list = Question.objects.order_by('-create_date')
@@ -59,5 +60,23 @@ def question_create(request):
             return redirect('pybo:index')
     else:
         form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id = question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
